3f47ddaf58bbe19c8a9d5b258d907db8d4f0a9fd
Hong Phuc <hongphuc.dthp@gmail.com>
2022-01-30 23:23:10 +0700

    add nodemon and docker-compose

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const ronin = require('ronin-server');
 const mocks = require('ronin-mocks');
+const database = require('ronin-database');
+
+database.connect(process.env.CONNECTIONSTRING);
 
 const server = ronin.server();
+
+server.use('/foo', (req, res) => {
+	return res.json({ foo: 'bar' });
+});
 
 server.use('/', mocks.server(server.Router(), false, true));
 
